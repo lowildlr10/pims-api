1edeb0645fae2f1bac3b6d9ee85f3f77f324287f
Lowil Ray Delos Reyes <lowildlr10@gmail.com>
2025-03-10 02:03:29 +0800

    Updated render.com file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -10,7 +10,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --seed --force
 
 echo "Linking public storage..."
 php artisan storage:link
